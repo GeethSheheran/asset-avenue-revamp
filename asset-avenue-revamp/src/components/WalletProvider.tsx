8f62e6170318d346e2a53a14f0725d1cef2cea7e
Asadullah khan <asadmuhammad427@gmail.com>
2025-02-08 13:18:38 +0500

    commit

--- asset-avenue-revamp/src/components/WalletProvider.tsx
+++ asset-avenue-revamp/src/components/WalletProvider.tsx
@@ -1,7 +1,7 @@
 import React, { useMemo } from "react";
 import { ConnectionProvider, WalletProvider } from "@solana/wallet-adapter-react";
 import { WalletModalProvider } from "@solana/wallet-adapter-react-ui";
-import { PhantomWalletAdapter, SolflareWalletAdapter } from "@solana/wallet-adapter-wallets";
+import { CoinbaseWalletAdapter, LedgerWalletAdapter, PhantomWalletAdapter, SolflareWalletAdapter, TorusWalletAdapter, TrustWalletAdapter } from "@solana/wallet-adapter-wallets";
 import { clusterApiUrl } from "@solana/web3.js";
 
 import "@solana/wallet-adapter-react-ui/styles.css"; // Import default styles
@@ -10,7 +10,14 @@ const WalletContextProvider: React.FC<{ children: React.ReactNode }> = ({ childr
     const network = clusterApiUrl("devnet"); // Use 'mainnet-beta' for production
 
     const wallets = useMemo(
-        () => [new PhantomWalletAdapter(), new SolflareWalletAdapter()],
+        () => [
+            new PhantomWalletAdapter(),
+            new SolflareWalletAdapter(),
+            new TrustWalletAdapter(),
+            new TorusWalletAdapter(),
+            new LedgerWalletAdapter(),
+            new CoinbaseWalletAdapter()
+        ],
         []
     );
 
